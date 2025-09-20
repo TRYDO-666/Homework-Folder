a7e3e9208c8699331a03e34d93c6a70eff0aed81
TRYDO-666 <hplaptopacharya@gmail.com>
2025-09-20 12:56:03 +0530

    Reorganize SQL script to insert book details before queries

--- Day3hw.sql
+++ Day3hw.sql
@@ -1,5 +1,24 @@
- SELECT DISTINCT genre FROM books_table;
- SELECT * FROM books_table WHERE stock_status = 'In stock' AND price < 400;
+INSERT INTO book_details (title, author, price, genre, stock_status)
+VALUES ('The Great Gatsby', 'F. Scott Fitzgerald', 350, 'Fiction','Out of Stock');
+
+INSERT INTO book_details (title, author, price, genre, stock_status)
+VALUES ('Cosmos', 'Carl Sagan', 500, 'Science','In Stock');
+
+INSERT INTO book_details (title, author, price, genre, stock_status)
+VALUES ('The Da Vinci Code', 'Dan Brown', 450, 'Mystery','Out of Stock');
+
+INSERT INTO book_details (title, author, price, genre, stock_status)
+VALUES ('The Anarchy', 'William Dalrymple', 600, 'History','In Stock');
+
+INSERT INTO book_details (title, author, price, genre, stock_status)
+VALUES ('To Kill a Mockingbird', 'Harper Lee', 300, 'Fiction','Out of Stock');
+
+SELECT DISTINCT genre FROM books_table;
+
+SELECT * FROM books_table WHERE stock_status = 'In stock' AND price < 400;
+
 SELECT * FROM books_table WHERE stock_status = 'Out of stock' OR price > 700;
+
 SELECT title,price, (price * 1.10) AS '10% price GST' FROM books_table;
- SELECT title, price, stock_status FROM books_table ORDER BY price DESC;
+
+SELECT title, price, stock_status FROM books_table ORDER BY price DESC;
